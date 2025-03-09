a04f9805a24c30569ce114d8361904e1f6d9208d
maikol garrido <104582495+Maikol2503@users.noreply.github.com>
2025-03-09 20:28:13 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: false,
-    openRouterApiKey: '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',
+    openRouterApiKey: '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',
     openRouterBaseUrl: 'https://openrouter.ai/api/v1'
 };
